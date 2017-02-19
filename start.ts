3e2204c97f0cdc8fb391adeae51423808218f52a
Mustafa Muhammad Saad <eng.mustafa.muhammad@gmail.com>
2017-02-19 14:11:26 +0200

    first commit

---
+++ start.ts
@@ -0,0 +1,13 @@
+// you can define varriable as
+var num1 = 7;
+var num2 : number;
+num2 = 9;
+var num3 :  number = 7;
+
+
+/// type inference
+var x = 7; // number
+var y = x + 1; //number
+var z = "mustafa"; // string
+var xx = y + x; // string
+//var yy : number = y + z; // error
